246435a892f025f4c5a55dbe6d63f2be3891c769
micah <msmith@commonwealthretirement.com>
2023-11-03 14:35:16 -0400

    add diagnostic signs to neo-tree

--- nvimlua/lua/plugins/neo-tree.lua
+++ nvimlua/lua/plugins/neo-tree.lua
@@ -15,10 +15,15 @@ M.keys = {
 }
 
 M.opts = function()
+  vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+  vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+  vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+  vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵 ", texthl = "DiagnosticSignHint" })
+
   return {
     filesystem = {
-       hijack_netrw_behavior = "open_default",
-       use_libuv_file_watcher = true
+      hijack_netrw_behavior = "open_default",
+      use_libuv_file_watcher = true
     }
   }
 end
